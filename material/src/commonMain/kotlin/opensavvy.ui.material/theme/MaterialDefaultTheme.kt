8ee765bf6bf8dab1496d34d2023ed364d9af4a89
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-11-01 16:22:33 +0100

    feat(material): Access both variants of material themes

--- material/src/commonMain/kotlin/opensavvy.ui.material/theme/MaterialDefaultTheme.kt
+++
@@ -1,9 +0,0 @@
-package opensavvy.ui.material.theme
-
-import opensavvy.ui.core.theme.ColorTheme
-import opensavvy.ui.core.theme.Theme
-
-object MaterialDefaultTheme : Theme {
-	override val color: ColorTheme
-		get() = Palette.default.toLightTheme()
-}
