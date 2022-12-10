0326b16fee41ac675674d52e8509610754023c79
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-12-10 17:36:39 +0100

    feat(demo): Showcase the progress indicator
    See #49

---
+++ demo/src/commonMain/kotlin/opensavvy/decouple/demo/components/Progress.kt
@@ -0,0 +1,56 @@
+package opensavvy.decouple.demo.components
+
+import androidx.compose.runtime.*
+import kotlinx.coroutines.delay
+import opensavvy.decouple.core.basic.AssistChip
+import opensavvy.decouple.core.basic.ChipGroup
+import opensavvy.decouple.core.basic.ProgressIndicator
+import opensavvy.decouple.core.basic.Text
+import opensavvy.decouple.core.layout.Column
+import opensavvy.decouple.core.layout.Row
+import opensavvy.decouple.core.progression.launch
+import opensavvy.state.Progression
+import opensavvy.state.ProgressionReporter.Companion.report
+
+@Composable
+fun Progression() = Column {
+	Text("Progression indicators notify the user about on-going work.")
+
+	val scope = rememberCoroutineScope()
+
+	var unquantified: Progression by remember { mutableStateOf(Progression.Done) }
+	var quantified: Progression by remember { mutableStateOf(Progression.Done) }
+
+	Row {
+		Text("Unquantified:")
+		ProgressIndicator(unquantified)
+	}
+
+	Row {
+		Text("Quantified:")
+		ProgressIndicator(quantified)
+	}
+
+	Text("States:")
+	ChipGroup {
+		AssistChip(
+			onClick = {
+				scope.launch(onProgress = { unquantified = it }) {
+					repeat(100) {
+						delay(100)
+						report(Progression.loading())
+					}
+				}
+				scope.launch(onProgress = { quantified = it }) {
+					repeat(100) {
+						delay(100)
+						report(Progression.loading(it / 100.0))
+					}
+				}
+			},
+			enabled = unquantified is Progression.Done && quantified is Progression.Done
+		) {
+			Text("Start the operation")
+		}
+	}
+}
