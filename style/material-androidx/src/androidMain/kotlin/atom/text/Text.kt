009702693be7cc5b3472071a7a9a939788e173ba
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-12-17 12:18:00 +0100

    feat(material): Implemented the text component for Android
    See #37

---
+++ style/material-androidx/src/androidMain/kotlin/atom/text/Text.kt
@@ -0,0 +1,14 @@
+package opensavvy.decouple.material.androidx.atom.text
+
+import androidx.compose.runtime.Composable
+import opensavvy.decouple.core.atom.text.Texts
+import androidx.compose.material3.Text as M3Text
+
+object MATexts : Texts {
+
+	@Composable
+	override fun Text(text: String) {
+		M3Text(text)
+	}
+
+}
