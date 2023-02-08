0823f8c70aab7983c9e6f6a2027df6b8746fa7fa
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-02-08 18:03:34 +0100

    feat(testing): Select between automatic or manual control of the test application

--- style/testing/src/commonMain/kotlin/execution/TestApplication.kt
+++ style/testing/src/commonMain/kotlin/execution/TestApplication.kt
@@ -5,14 +5,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Composition
 import androidx.compose.runtime.Recomposer
 import androidx.compose.runtime.snapshots.Snapshot
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.job
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.yield
+import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import opensavvy.decouple.core.UI.Companion.Install
 import opensavvy.decouple.testing.Component
 import opensavvy.decouple.testing.TestUI
 import opensavvy.decouple.testing.node.NodeTree
+import opensavvy.logger.Logger.Companion.trace
+import opensavvy.logger.loggerFor
 
 /**
  * Executes a composable function in a test environment where no UI is emitted.
@@ -27,16 +28,47 @@ class TestApplication internal constructor(
 	private val root: ExecutableNode,
 ) : NodeTree by root {
 
+	private val log = loggerFor(this)
+	private val executionLock = Mutex()
+
+	// DO NOT CALL WITHOUT HOLDING THE EXECUTION LOCK
+	private suspend fun recomposeUnlocked() {
+		log.trace { "The test application is recomposing…" }
+		yield()
+		Snapshot.sendApplyNotifications()
+		clock.sendFrame(1)
+		recomposer.awaitIdle()
+	}
+
 	/**
 	 * Allows the application to respond to events once.
 	 *
 	 * The test application is frozen in time, actions executed have no impact until this method is called.
 	 */
-	suspend fun recompose() {
-		yield()
-		Snapshot.sendApplyNotifications()
-		clock.sendFrame(1)
-		recomposer.awaitIdle()
+	suspend fun recompose() = executionLock.withLock("recompose") {
+		recomposeUnlocked()
+	}
+
+	fun CoroutineScope.recomposeAutomatically(delayMillis: Long = 100) = launch {
+		while (true) {
+			executionLock.withLock("recomposeAutomatically") { recomposeUnlocked() }
+			delay(delayMillis)
+		}
+	}
+
+	/**
+	 * Executes [block] while the application is paused.
+	 *
+	 * Inside this block,
+	 */
+	suspend fun paused(block: suspend TestApplication.() -> Unit) {
+		println(this)
+
+		executionLock.withLock("paused") {
+			block()
+
+			recomposeUnlocked()
+		}
 	}
 
 	override fun toString() = root.toString()
@@ -60,7 +92,7 @@ class TestApplication internal constructor(
  *     // Declare our UI
  *     // You should either use backgroundScope, or create your own scope that you later cancel to stop the application
  *     val ui = backgroundScope.compose {
- *         // Inside this lambda, you can use any composable that follows the Decouple structure
+ *         // Inside this lambda, any composable that follows the Decouple structure can be called.
  *         // It's not possible to test other UI-emitting composables, for example those from Jetpack Compose
  *         // or Compose for Web, as they are hard-coded to build real UI trees.
  *
@@ -73,24 +105,43 @@ class TestApplication internal constructor(
  *         }
  *     }
  *
- *     // Display the entire UI hierarchy
+ *     // Print the entire UI hierarchy
  *     println(ui)
  *
- *     // Access the component state
- *     val text = ui.find(Text)
- *     assertEquals("Counter: 0", text.text)
+ *     // By default, the UI is running asynchronously for the duration of the test
+ *     // To ensure our assertions do not impact each other, we must temporarily pause it
+ *     ui.paused {
+ *         // We can now assert the state of the components
+ *         assertEquals("Counter: 0", find(Text).text)
  *
- *     // Trigger events
- *     ui.find(Button).click()
- *     ui.recompose() // let the UI process events
+ *         // We can also trigger events
+ *         find(Button).click()
+ *         // Because the UI is currently paused, the events are queued but not executed yet.
+ *         assertEquals("Counter: 0", find(Text).text)
+ *     }
  *
- *     // Check the new state
- *     val text = ui.find(Text)
- *     assertEquals("Counter: 1", text.text)
+ *     // The UI is now running, the queued events are being executed
+ *
+ *     // Let's pause it again to check the results…
+ *     ui.paused {
+ *         assertEquals("Counter: 1", find(Text).text)
+ *     }
  * }
  * ```
+ *
+ * ### Manual recomposition
+ *
+ * By default, the application is running between [paused][TestApplication.paused] calls.
+ * For more fine-tuned control over the execution, set the optional parameter [manualRecomposition] to `true`.
+ *
+ * In that case, the application will be stopped by default.
+ * To execute it step-by-step, call [recompose][TestApplication.recompose].
+ * To start it, call [recomposeAutomatically][TestApplication.recomposeAutomatically].
  */
-fun CoroutineScope.compose(content: @Composable () -> Unit): TestApplication {
+fun CoroutineScope.compose(
+	manualRecomposition: Boolean = false,
+	content: @Composable () -> Unit,
+): TestApplication {
 	val clock = BroadcastFrameClock()
 
 	val root = ExecutableNode("Root", isSlot = true)
@@ -113,9 +164,15 @@ fun CoroutineScope.compose(content: @Composable () -> Unit): TestApplication {
 		}
 	}
 
-	return TestApplication(
+	val application = TestApplication(
 		recomposer,
 		clock,
 		root,
 	)
+
+	if (!manualRecomposition) {
+		launch { with(application) { recomposeAutomatically() } }
+	}
+
+	return application
 }
