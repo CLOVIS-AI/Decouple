7dcaad4638c57e62f213d58bfc7fa2d6bfe9a9a7
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-04-19 14:04:51 +0200

    refactor(material-tailwind): Vertically align the global navigation bar

--- style/material-tailwind/src/main/kotlin/layout/Navigation.kt
+++ style/material-tailwind/src/main/kotlin/layout/Navigation.kt
@@ -3,10 +3,7 @@ package opensavvy.decouple.material.tailwind.layout
 import androidx.compose.runtime.*
 import opensavvy.decouple.core.atom.actionable.Button
 import opensavvy.decouple.core.atom.text.Text
-import opensavvy.decouple.core.layout.Arrangement
-import opensavvy.decouple.core.layout.Column
 import opensavvy.decouple.core.layout.Navigation
-import opensavvy.decouple.core.layout.Row
 import opensavvy.decouple.core.navigation.NavigationMenu
 import org.jetbrains.compose.web.dom.Div
 
@@ -22,54 +19,96 @@ object MTNavigation : Navigation {
 		selected: NavigationMenu<P>,
 		onSelect: (NavigationMenu.Page<P>) -> Unit,
 		currentContents: @Composable () -> Unit,
-	) = Row(Arrangement.Start) {
+	) = Div(
+		{
+			classes("flex", "flex-row", "gap-2", "justify-start", "items-stretch", "h-full", "w-full")
+		}
+	) {
 		var open by remember { mutableStateOf<NavigationMenu.Menu<P>?>(null) }
 
 		Div(
 			{
 				onMouseLeave { open = null }
+				classes("h-full")
 			}
 		) {
-			Row {
-				// Always-open panel
-				Column {
-					for (child in menu.children) {
-						Div(
-							{
-								onMouseEnter {
-									open =
-										if (child is NavigationMenu.Menu) child
-										else null
-								}
-							}
-						) {
-							Button(
-								onClick = {
-									child.firstPage()?.also(onSelect)
-								},
-								enabled = selected != child,
-							) { Text(child.title) }
-						}
-					}
+			Div(
+				{
+					classes("h-full", "flex", "flex-row")
 				}
+			) {
+				// Always-open panel
+				Rail(menu, onOpen = { open = it }, selected, onSelect)
 
 				// Sub-panel
-				Column {
-					val open = open
-					if (open != null) {
-						for (child in open.children) {
-							Button(
-								onClick = {
-									child.firstPage()?.also(onSelect)
-								},
-								enabled = selected != child,
-							) { Text(child.title) }
-						}
-					}
+				open?.let {
+					Submenu1(it, selected, onSelect)
 				}
 			}
 		}
 
-		currentContents()
+		Div(
+			{
+				classes("overflow-auto", "grow")
+			}
+		) {
+			currentContents()
+		}
+	}
+
+	@Composable
+	private fun <P> Rail(
+		menu: NavigationMenu.Menu<P>,
+		onOpen: (NavigationMenu.Menu<P>?) -> Unit,
+		selected: NavigationMenu<P>,
+		onSelect: (NavigationMenu.Page<P>) -> Unit,
+	) {
+		Div(
+			{
+				classes("h-full", "flex", "flex-col", "justify-center", "items-stretch", "gap-4")
+			}
+		) {
+			for (child in menu.children) key(child) {
+				Div(
+					{
+						onMouseEnter {
+							onOpen(
+								if (child is NavigationMenu.Menu) child
+								else null
+							)
+						}
+					}
+				) {
+					Button(
+						onClick = {
+							child.firstPage()?.also(onSelect)
+						},
+						enabled = selected != child,
+					) { Text(child.title) }
+				}
+			}
+		}
+	}
+
+	@Composable
+	private fun <P> Submenu1(
+		open: NavigationMenu.Menu<P>,
+		selected: NavigationMenu<P>,
+		onSelect: (NavigationMenu.Page<P>) -> Unit,
+	) {
+		Div(
+			{
+				classes("h-full", "flex", "flex-col", "justify-center", "items-stretch", "gap-4")
+			}
+		) {
+			for (child in open.children) key(child) {
+				Button(
+					onClick = {
+						child.firstPage()?.also(onSelect)
+					},
+					enabled = selected != child,
+				) { Text(child.title) }
+			}
+		}
 	}
 }
