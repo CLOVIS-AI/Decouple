49271ef3009ae46a7410bb38f3ec5e5a7e1cc588
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-06-22 10:00:56 +0200

    refactor(material-tailwind): Factorize common code between LazyColumn and LazyRow

--- styles/style-material-tailwind/src/jsMain/kotlin/layout/LazyLayouts.kt
+++ styles/style-material-tailwind/src/jsMain/kotlin/layout/LazyLayouts.kt
@@ -6,6 +6,7 @@ import opensavvy.decouple.core.layout.*
 import org.jetbrains.compose.web.dom.Div
 import web.dom.document
 import web.dom.observers.IntersectionObserver
+import kotlin.math.max
 import kotlin.random.Random
 import kotlin.random.nextUInt
 
@@ -16,57 +17,12 @@ object MTLazyLayouts : LazyLayouts {
 		vertical: Arrangement,
 		alignment: Alignment,
 		content: LazyLayouts.LazyColumnScope.() -> Unit,
-	) {
-		val elements: List<() -> List<LazyItem>> = remember(content) {
-			val elements = ArrayList<() -> List<LazyItem>>()
-
-			object : LazyLayouts.LazyColumnScope {
-				override fun item(key: Any?, block: @Composable () -> Unit) {
-					elements += { listOf(LazyItem(key) { block() }) }
-				}
-
-				override fun items(count: Int, key: (index: Int) -> Any, block: @Composable (index: Int) -> Unit) {
-					elements += {
-						List(count) {
-							LazyItem(key(it)) { block(it) }
-						}
-					}
-				}
-
-				override fun <K : Any> items(items: Iterable<K>, key: (item: K) -> Any, block: @Composable (item: K) -> Unit) {
-					elements += {
-						items.map {
-							LazyItem(key(it)) { block(it) }
-						}
-					}
-				}
-			}.apply(content)
-
-			elements
-		}
-
-		var nextIndex by remember(content) { mutableStateOf(0) }
-
-		val items = remember(content) { mutableStateListOf<LazyItem>() }
-
-		Column(vertical, alignment) {
-			for (item in items) {
-				key(item.key) {
-					item.block()
-				}
-			}
-
-			key(content, nextIndex) {
-				VisibilityDetector(
-					onVisible = {
-						if (nextIndex <= elements.lastIndex) {
-							Snapshot.withMutableSnapshot {
-								items += elements[nextIndex]()
-								nextIndex++
-							}
-						}
-					}
-				)
+	) = key(content) {
+		LazyLinearLayout(
+			generator = { concreteLazyColumn(content) },
+		) { items ->
+			Column(vertical, alignment) {
+				items()
 			}
 		}
 	}
@@ -76,9 +32,13 @@ object MTLazyLayouts : LazyLayouts {
 		horizontal: Arrangement,
 		alignment: Alignment,
 		content: LazyLayouts.LazyRowScope.() -> Unit,
-	) {
-		Row(horizontal, alignment) {
-			// TODO
+	) = key(content) {
+		LazyLinearLayout(
+			generator = { concreteLazyRow(content) },
+		) { items ->
+			Row(horizontal, alignment) {
+				items()
+			}
 		}
 	}
 }
@@ -87,6 +47,79 @@ private class LazyItem(
 	val key: Any?,
 	val block: @Composable () -> Unit,
 )
+
+private typealias LazyItemsGenerator = () -> List<LazyItem>
+
+private class LazyAdapter(val elements: MutableList<LazyItemsGenerator>) : LazyLayouts.LazyColumnScope, LazyLayouts.LazyRowScope {
+	override fun item(key: Any?, block: @Composable () -> Unit) {
+		elements += { listOf(LazyItem(key) { block() }) }
+	}
+
+	override fun items(count: Int, key: (index: Int) -> Any, block: @Composable (index: Int) -> Unit) {
+		elements += {
+			List(count) {
+				LazyItem(key(it)) { block(it) }
+			}
+		}
+	}
+
+	override fun <K : Any> items(items: Iterable<K>, key: (item: K) -> Any, block: @Composable (item: K) -> Unit) {
+		elements += {
+			items.map {
+				LazyItem(key(it)) { block(it) }
+			}
+		}
+	}
+}
+
+private fun concreteLazyColumn(content: LazyLayouts.LazyColumnScope.() -> Unit): List<LazyItemsGenerator> =
+	ArrayList<LazyItemsGenerator>()
+		.also { LazyAdapter(it).apply(content) }
+
+private fun concreteLazyRow(content: LazyLayouts.LazyRowScope.() -> Unit): List<LazyItemsGenerator> =
+	ArrayList<LazyItemsGenerator>()
+		.also { LazyAdapter(it).apply(content) }
+
+@Composable
+private fun LazyLinearLayout(
+	generator: () -> List<LazyItemsGenerator>,
+	layout: @Composable (@Composable () -> Unit) -> Unit,
+) {
+	val elements = remember { generator() }
+
+	var nextIndex by remember { mutableStateOf(0) }
+	val items = remember { mutableStateListOf<LazyItem>() }
+
+	val visibilityDetector = remember(nextIndex, elements) {
+		{
+			if (nextIndex <= elements.lastIndex) {
+				Snapshot.withMutableSnapshot {
+					items += elements[nextIndex]()
+					nextIndex++
+				}
+			}
+		}
+	}
+
+	layout {
+		if (items.isEmpty()) {
+			VisibilityDetector(visibilityDetector)
+		}
+
+		for ((i, item) in items.withIndex()) {
+			Div {
+				// Add the visibility detector as part of an element to avoid duplicate margins
+				if (i == max(0, items.size - 5)) {
+					VisibilityDetector(visibilityDetector)
+				}
+
+				key(item.key) {
+					item.block()
+				}
+			}
+		}
+	}
+}
 
 @Composable
 private fun VisibilityDetector(onVisible: () -> Unit) {
